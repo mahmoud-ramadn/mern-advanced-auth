464b63a9fef171a015cb5e82c347c1b59e46a095
mahmoud-ramadn <ahmedshawareb567@gmail.com>
2025-12-03 13:32:38 +0200

    fix urls

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -64,9 +64,7 @@ export const login = async (req, res) => {
         .status(400)
         .json({ success: false, message: "Invalid credentials" });
     }
-
     const isPasswordValid = await bcryptjs.compare(password, user.password);
-
     if (!isPasswordValid) {
       return res
         .status(400)
@@ -74,18 +72,24 @@ export const login = async (req, res) => {
     }
 
     generateTokenAndSetCookie(res, user._id);
+
     user.lastLogin = new Date();
     await user.save();
 
-    return res.status(201).json({
+    res.status(200).json({
       success: true,
-      message: "User logged in successfully",
-      user: { ...user._doc, password: undefined },
+      message: "Logged in successfully",
+      user: {
+        ...user._doc,
+        password: undefined,
+      },
     });
   } catch (error) {
-    return res.status(500).json({ success: false, message: error.message });
+    console.log("Error in login ", error);
+    res.status(400).json({ success: false, message: error.message });
   }
 };
+
 
 export const logout = async (req, res) => {
   res.clearCookie("jwt");
